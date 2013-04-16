7a8c1d5c5399f33de9d4da0e32699a9b267b2f5e
Phill Baker <phillbaker@retrodict.com>
2013-04-15 22:51:55 -0400

    Comment out some aliases, add to bootstrap prompt + add TODO for desktop background.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,12 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+  for file in `ls ~/*.example`; do mv "${file}" "${file%.example}"; done
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -p "This may overwrite existing files in your home directory. It will copy examples to their non-example locations. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
