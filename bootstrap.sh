e8a79e4ccf8767474c38a9d26e8acff92c08952d
Phill Baker <phill@generalassemb.ly>
2013-06-03 00:04:12 -0400

    Fixes for example files + clean up.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,26 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-  for file in `ls ~/*.example`; do mv "${file}" "${file%.example}"; done
+}
+function doExamples() {
+  for file in `ls ~/.*.example`; do mv "${file}" "${file%.example}"; done
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
+  doExamples
 else
 	read -p "This may overwrite existing files in your home directory. It will copy examples to their non-example locations. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
+
+  read -p "Copy examples to their non-example locations? Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doExamples
+  fi
 fi
 unset doIt
+unset doExamples
 source ~/.bash_profile
